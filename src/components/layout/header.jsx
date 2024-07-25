b0b18ccd1c9f47c75f52bdedf266f7941f6db7d4
amro22397 <amroalmutasim22@gmail.com>
2024-07-25 08:53:33 +0400

    created header

---
+++ src/components/layout/header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div>
+      <header className="flex items-center justify-between">
+      <Link className="text-red-500 font-semibold text-2xl" href=''>ST PIZZA</Link>
+
+      <nav className="flex items-center gap-8">
+        <Link href={''}>Home</Link>
+        <Link href={''}>Menu</Link>
+        <Link href={''}>About</Link>
+        <Link href={''}>Contact</Link>
+        <Link href={''} className="bg-red-600
+        rounded-full text-white px-8 py-2">Login</Link>
+      </nav>
+    </header>
+    </div>
+  )
+}
+
+export default header
