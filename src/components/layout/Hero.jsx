e22099dea7bce551a5e296bc736132f0d83c9d1b
amro22397 <amroalmutasim22@gmail.com>
2024-07-25 16:07:16 +0400

    styling hero nice

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -4,32 +4,39 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <section className='hero'>
+<section className='hero'>
     <div className='py-12'>
-        <h1 className="text-4xl font-semibold">Everything is better with a Pizza</h1>
+        <h1 className="text-4xl font-semibold">Everything<br/>
+         is better<br/>
+          with a&nbsp;
+          <span className='text-red-600'>
+            Pizza
+            </span></h1>
         <p className="my-4 text-gray-500">
             Pizza is the missing piece that makes every
             complete.
         </p>
-    </div>
 
-    <div className="flex gap-4 text-sm">
+        <div className="flex gap-4 text-sm">
         <button className='bg-red-600 gap-2 text-white px-5 py-2
-        rounded-full flex flex-row'>
+        rounded-full flex flex-row items-center'>
             Order now
             <Right/>
             </button>
-        <button className='flex gap-2 py-2
-        text-gray-600 font-semibold'>Learn more 
+        <button className='gap-2 py-2
+        text-gray-600 font-semibold 
+        flex flex-row rounded-full items-center'>
+            Learn more 
         <Right/>
         </button>
+    </div>
     </div>
 
     <div className="relative">
         <Image src={'/pizza.png'} layout={'fill'}
         objectFit={'contain'} alt={'pizza'} />
     </div>
-    </section>
+</section>
   )
 }
 
---
+++ src/components/layout/HomeMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeMenu = () => {
+  return (
+    <div>
+      home
+    </div>
+  )
+}
+
+export default HomeMenu
