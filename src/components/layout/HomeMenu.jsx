d36ae744639c43cdede4963a3d7e481a6cd9eca1
amro22397 <amroalmutasim22@gmail.com>
2024-07-25 16:38:09 +0400

    add homemenu file

--- src/components/layout/HomeMenu.jsx
+++ src/components/layout/HomeMenu.jsx
@@ -29,17 +29,7 @@ const HomeMenu = () => {
         </div>
 
         <div className="grid grid-cols-3 gap-4">
-            <div className="bg-gray-200 p-4 rounded-lg text-center">
-                <img src="/pizza.png" alt="pizza" />
-                <h4 className="font-semibold text-xl my-3">Pepperoni Pizza</h4>
-                <p className="text-gray-500 text-sm">
-                    Lorem ipsum dolor sit amet ipsum dolor sit amet ipsum dolor sit amet
-                    ipsum dolor sit amet.
-                </p>
-                <p className="mt-3 text-xl font-semibold" style={{fontFamily: 'arial'}}>$ 12</p>
-                <button className="mt-4 bg-red-600 text-white
-                rounded-full px-8 py-2">Add to cart</button>
-            </div>
+            
         </div>
     </section>
   )
