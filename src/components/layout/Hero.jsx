70dbeaf6eecd2b5733fc991427d16883aec21f37
amro22397 <amroalmutasim22@gmail.com>
2024-07-25 15:50:46 +0400

    hero edit and add public files

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <section>
-    <div>
+    <section className='hero'>
+    <div className='py-12'>
         <h1 className="text-4xl font-semibold">Everything is better with a Pizza</h1>
         <p className="my-4 text-gray-500">
             Pizza is the missing piece that makes every
@@ -13,13 +13,16 @@ const Hero = () => {
         </p>
     </div>
 
-    <div className="flex flex-row gap-4">
+    <div className="flex gap-4 text-sm">
         <button className='bg-red-600 gap-2 text-white px-5 py-2
         rounded-full flex flex-row'>
             Order now
             <Right/>
             </button>
-        <button>Learn more</button>
+        <button className='flex gap-2 py-2
+        text-gray-600 font-semibold'>Learn more 
+        <Right/>
+        </button>
     </div>
 
     <div className="relative">
