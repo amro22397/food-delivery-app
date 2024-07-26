38393e8f421c7636b2cc9a4816dbd024c7c7cb2c
amro22397 <amroalmutasim22@gmail.com>
2024-07-26 22:13:22 +0400

    add about and contact section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Header from "@/components/layout/Header";
 import Hero from "@/components/layout/Hero";
 import HomeMenu from "@/components/layout/HomeMenu";
+import SectionHeaders from "@/components/layout/SectionHeaders";
 
 export default function Home() {
   return (
@@ -8,6 +9,42 @@ export default function Home() {
     <Header />
     <Hero />
     <HomeMenu />
+    <section className="text-center my-16">
+      <SectionHeaders 
+      subHeader={'Our story'}
+      mainHeader={'About us'}
+      />
+      <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col
+      gap-4">
+        <p className="">
+          Lorem ipsum dolor sit amet, ipsum dolor sit amet
+          ipsum dolor sit amet ipsum dolor sit amet
+          ipsum dolor sit amet ipsum dolor sit amet ipsum dolor sit amet
+          ipsum dolor sit amet ipsum dolor sit amet ipsum dolor sit amet.
+        </p>
+        <p className="max-w-2xl mx-auto mt-4">
+          Lorem ipsum dolor sit amet, ipsum dolor sit amet
+          ipsum dolor sit amet ipsum dolor sit amet
+          ipsum dolor sit amet ipsum dolor sit amet ipsum dolor sit amet
+          ipsum dolor sit amet ipsum dolor sit amet ipsum dolor sit amet.
+        </p>
+      </div>
+      </section>   
+      <section className="text-center my-4">
+        <SectionHeaders 
+        subHeader={"Don't hesitate"}
+        mainHeader={'Contact Us'}
+        />
+        <div className="mt-8">
+          <a href="tel:+96879335801" className="text-4xl">
+            +968 79335801
+          </a>
+        </div>
+      </section>
+      <footer className="border-t p-8 text-center text-gray-500
+      mt-16">
+        &copy; {new Date().getFullYear()} All rights reserved
+      </footer>
     </>
   );
 }
